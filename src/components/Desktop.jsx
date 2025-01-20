9f4de1a32822e28f6eb7920e804c6cd9989d3233
Jérémy Girard <oqaislenning@gmail.com>
2025-01-20 17:23:14 +0100

    feat: update desktop animations

--- src/components/Desktop.jsx
+++ src/components/Desktop.jsx
@@ -1,28 +1,54 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { useWindowContext } from '../assets/scripts/WindowContext';
 import { Explorer108 } from "@react95/icons";
 
 export const Desktop = () => {
-    const { windows, toggleWindow } = useWindowContext();
+    const { toggleWindow } = useWindowContext();
+
+    const [activeIcon, setActiveIcon] = useState(null);
 
     const handleToggleIcon = (iconId) => {
         setActiveIcon((prev) => (prev === iconId ? null : iconId));
     };
-    const [activeIcon, setActiveIcon] = useState(null);
 
-    const handleDoubleClick = () => {
-        toggleWindow('CV');
+    const handleDoubleClick = (windowName) => {
+        toggleWindow(windowName);
     };
 
+    const containerRef = useRef(null);
+
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (containerRef.current && !containerRef.current.contains(event.target)) {
+                setActiveIcon(null); // Désactive toutes les icônes
+            }
+        };
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []);
+
     return (
-        <div className="desktop-icons">
+        <div className="desktop-container" ref={containerRef}>
             <div
-                className={activeIcon === 1 ? "active-icon" : "inactive-icon"}
+                className={activeIcon === 1 ? "active-icon desktop-icon" : "inactive-icon desktop-icon"}
                 onClick={() => handleToggleIcon(1)}
-                onDoubleClick={handleDoubleClick}
+                onDoubleClick={() => handleDoubleClick('RecycleBin')}
             >
                 <Explorer108 variant="32x32_4" />
                 <p>Recycle Bin</p>
+            </div>
+            <div
+                className={activeIcon === 2 ? "active-icon desktop-icon" : "inactive-icon desktop-icon"}
+                onClick={() => handleToggleIcon(2)}
+                onDoubleClick={() => handleDoubleClick('CV')}
+            >
+                <Explorer108 variant="32x32_4" />
+                <p>Curriculum Vitae</p>
             </div>
         </div>
     );
