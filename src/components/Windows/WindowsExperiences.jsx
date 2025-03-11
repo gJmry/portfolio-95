93ab8fc4d647ad400bcd7c9c652e6f51d395bfa0
Jérémy Girard <oqaislenning@gmail.com>
2025-03-11 16:31:16 +0100

    update json

--- src/components/Windows/WindowsExperiences.jsx
+++ src/components/Windows/WindowsExperiences.jsx
@@ -25,12 +25,13 @@ export function WindowsExperiences() {
     const [selectedExp, setSelectedExp] = useState(null);
 
     const experiences = [
-        {year: '2022-2025', title: '📎 BUT Computer Science Lyon 1', desc: ''},
-        {year: '2022-2023', title: '📎 Association Secretary (BDE Info)', desc: 'Note-taking, administrative management'},
-        {year: '2023', title: '🛠 Full Stack Developer Internship', desc: 'Web development internship, first professional project in Symfony PHP at Onyl Rocks'},
-        {year: '2024', title: '👔 Co-President of an Association (Student Club)', desc: 'Event organization and team management'},
-        {year: '2023-2024', title: '📌 Full Stack Developer Apprenticeship', desc: 'Web development apprenticeship, using Symfony & Javascript, at Onyl Rocks'},
-        {year: '2024-2025', title: '📝 Association Treasurer (Student Club)', desc: 'Managing budgets and finances of a student association'},
+        {year: '2022-2025', title: '🎓 BUT Computer Science Lyon 1', desc: 'Pursuing a Bachelor in Technology focused on Computer Science, with coursework covering software development, algorithms, and network systems.'},
+        {year: '2022-2023', title: '🤝 Association Secretary (BDE Info)', desc: 'Responsible for keeping detailed minutes of meetings, overseeing administrative tasks, and facilitating communication within the student body.'},
+        {year: '2023', title: '🖥️ Full Stack Developer Internship', desc: 'Completed an immersive web development internship where I worked on my first professional project using Symfony PHP at Onyl Rocks, enhancing my programming skills and teamwork experience.'},
+        {year: '2023-2024', title: '👨‍💻 Full Stack Developer Apprenticeship', desc: 'Engaged in a hands-on apprenticeship focused on web development with a strong emphasis on Symfony and JavaScript at Onyl Rocks, further developing my coding proficiency and real-world application capabilities.'},
+        {year: '2024-2026', title: '🔗 Elected IUT UFR Representative', desc: 'Serving as an elected representative for the IUT UFR, advocating for student interests and fostering communication between students and faculty.'},
+        {year: '2024', title: '🌟 Co-President of a Student Association', desc: 'Led a team in organizing various student events, promoting engagement, and fostering community spirit within the university, while developing leadership and organizational skills.'},
+        {year: '2024-2025', title: '💰 Association Treasurer (Student Club)', desc: 'Oversaw the financial management of the student association, including budgeting and expense tracking, ensuring transparency and responsible spending.'}
     ];
 
     return (
