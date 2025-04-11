e05c73dfa478b58c0d2205a1efb409e28b496bcd
Jérémy Girard <oqaislenning@gmail.com>
2025-04-11 17:06:34 +0200

    feat: update background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,8 +5,11 @@ const diagonalMove = keyframes`
     0% {
         transform: translate(0, 0);
     }
-    100% {
+    50% {
         transform: translate(100px, -100px);
+    }
+    100% {
+        transform: translate(0, 0);
     }
 `;
 
@@ -19,8 +22,8 @@ const Text = styled.p`
 `;
 
 const getRandomPosition = () => {
-    const x = Math.random() * 100;
-    const y = Math.random() * 100;
+    const x = (Math.random() * 120)-20;
+    const y = (Math.random() * 120)-20;
     return { x, y };
 };
 
@@ -36,7 +39,7 @@ const RandomText = ({ text }) => {
     return <Text style={{ top: `${y}%`, left: `${x}%`, fontSize: `${fontSize}px` }}>{text}</Text>;
 };
 
-const Background = ({ text = "Jérémy Girard", count = 300 }) => {
+const Background = ({ text = "Jérémy Girard", count = 200 }) => {
     return (
         <div style={{ position: 'absolute', width: '100%', height: '100%' }}>
             {Array.from({ length: count }).map((_, index) => (
